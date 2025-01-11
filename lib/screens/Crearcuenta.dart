44151f9f796be809c10810e0ae3bf449b58a5de8
carlamoreno123 <carlamorenoc2005@gmail.com>
2025-01-11 12:41:17 +0100

    intentando assets

--- lib/screens/Crearcuenta.dart
+++ lib/screens/Crearcuenta.dart
@@ -7,166 +7,170 @@ class Crearcuenta extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            // Imagen agregada
-
-            SizedBox(height: 20),
-            Text(
-              '¡Comenzemos a crear tu cuenta!',
-              style: TextStyle(
-                fontSize: 30,
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/image.jpg'), // Ruta de la imagen
+              fit: BoxFit.cover, // Hace que la imagen ocupe toda la pantalla
             ),
-            SizedBox(height: 50),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Escribe aquí...',
-                    hintText: 'nombre de usuario',
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'nombre de',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-                Text(
-                  'usuario',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-            TextField(
-              obscureText: true, // Oculta la contraseña
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Escribe aquí...',
-                hintText: 'contraseña',
-              ),
-            ),
-            
-            
-            Text(
-                  'contraseña',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Escribe aquí...',
-                    hintText: 'nombre de usuario',
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'confirmar',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-                Text(
-                  'usuario',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-            TextField(
-              obscureText: true, 
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Escribe aquí...',
-                hintText: ' contraseña',
-              ),
-            ),
-            
-            
-            Text(
-                  'confirmar ',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
+          ),
+      
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
                   Text(
-                  'contraseña ',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
+                    '¡Comenzemos a crear tu cuenta!',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                
-              ],
-            ),
-            SizedBox(height: 70,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('¿Ya lo tienes?'),
-                 ElevatedButton(
-                onPressed: () {const Iniciosesion();},
-                child: Text(
-                  'Inicia sesion',
-                  style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ),
-              ],
-            )
-          
-        ]),
-        
-      ]),
-    )
-    );
+                  SizedBox(height: 50),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Escribe aquí...',
+                          hintText: 'nombre de usuario',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'nombre de',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      Text(
+                        'usuario',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextField(
+                          obscureText: true, // Oculta la contraseña
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Escribe aquí...',
+                            hintText: 'contraseña',
+                          ),
+                        ),
+                        Text(
+                          'contraseña',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Escribe aquí...',
+                                hintText: 'nombre de usuario',
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'confirmar',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                            Text(
+                              'usuario',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 30),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Escribe aquí...',
+                                hintText: ' contraseña',
+                              ),
+                            ),
+                            Text(
+                              'confirmar ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                            Text(
+                              'contraseña ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 70,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('¿Ya lo tienes?'),
+                            ElevatedButton(
+                              onPressed: () {
+                                const Iniciosesion();
+                              },
+                              child: Text(
+                                'Inicia sesion',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255)),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        )
+                      ]),
+                ]),
+          ));
     
   }
 }
